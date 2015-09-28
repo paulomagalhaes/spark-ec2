f8b9988593aa7f28e1d5ecf3ee8ed2c19a088169
Paulo Magalhaes <paulo.magalhaes@gmail.com>
2015-09-28 17:43:44 -0300

    proper dir for nutch

--- nutch/init.sh
+++ nutch/init.sh
@@ -1,6 +1,8 @@
+pushd /root > /dev/null
 wget https://s3-us-west-2.amazonaws.com/uberdata-public/nutch/apache-nutch-1.10-bin.tar.gz
 echo "Unpacking Nutch"
 tar xvzf apache-nutch-*.tar.gz > /tmp/nutch_spark.log
 rm apache-nutch-*.tar.gz
 mv apache-nutch-* nutch
 cp ephemeral-hdfs/conf/* nutch/conf
+popd > /dev/null
